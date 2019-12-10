5feae72d37ebf3127bd0cb0127fba9d38f8e7e6e
James Taylor <j.taylor@kainos.com>
2019-12-10 17:56:18 +0000

    Add value to know if food belongs to logged in user

--- src/resolvers/FoodItem.js
+++ src/resolvers/FoodItem.js
@@ -1,9 +1,20 @@
+const { getUserId } = require('../utils')
+
+
 const FoodItem = {
   createdBy: ({ id }, args, context) => {
     return context.prisma.foodItem({ id }).createdBy()
   },
   mealsPartOf: ({ id }, args, context) => {
     return context.prisma.foodItem({ id }).mealsPartOf()
+  },
+  createdByLoggedInUser: async ({ id }, args, context) => {
+    const userId = getUserId(context)
+    const createdByUser = await context.prisma.foodItem({ id }).createdBy()
+    if (createdByUser !== undefined && createdByUser !== null) {
+      return createdByUser.id === userId
+    }
+    return false
   }
 }
 
